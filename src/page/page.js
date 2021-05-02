370f07eebcdaa799f98973c92bd2567dca754a04
juan <juancraft12yt@gmail.com>
2021-05-01 22:03:59 -0300

    Create page

---
+++ src/page/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import About from "../components/about";
+import BrandPresentation from "../components/brand";
+
+export default function Page() {
+  return (
+    <>
+      <BrandPresentation />
+      <About />
+      <footer>footer</footer>
+    </>
+  );
+}
