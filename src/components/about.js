9358de6015d6f0aa0251a3af67182a1d20a3eae5
juan <juancraft12yt@gmail.com>
2021-05-01 22:03:51 -0300

    Create about component

---
+++ src/components/about.js
@@ -0,0 +1,106 @@
+import React from "react";
+import styled from "@emotion/styled";
+import icon from "../assets/trend.svg";
+
+const StyledSection = styled.section`
+  flex-grow: 1;
+  min-height: 100vh;
+  padding: 20px 5px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  background: -webkit-linear-gradient(#7d68ac, #c024ac);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 3rem;
+  text-align: center;
+  @media screen and (min-width: 600px) {
+    font-size: 3.5rem;
+  }
+`;
+
+const SubTitle = styled.h2`
+  font-size: 1.5rem;
+  text-align: center;
+  background: -webkit-linear-gradient(#7d68ac, #c024ac);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  padding: 10px;
+  margin-top: 10px;
+  @media screen and (min-width: 600px) {
+    font-size: 2rem;
+  }
+`;
+
+const CardTitle = styled.h3`
+  font-size: 1rem;
+  margin-bottom: 10px;
+  color: #c024ac;
+  @media screen and (min-width: 600px) {
+    font-size: 2rem;
+  }
+`;
+
+const AboutCard = styled.div`
+  margin: 10px;
+  padding: 10px;
+  @media screen and (min-width: 600px) {
+    max-width: 500px;
+  }
+`;
+
+const CardsContainer = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+`;
+const ImgIcon = styled.img`
+  width: 200px;
+  @media screen and (min-width: 768px) {
+    width: 300px;
+  }
+`;
+export default function About() {
+  return (
+    <StyledSection>
+      <div>
+        <Title>About Us</Title>
+        <SubTitle>
+          Founded in 1990 by Juan Avero,
+          <br /> the company never stopped growing until we became # 1 in
+          Argentina
+        </SubTitle>
+      </div>
+      <CardsContainer>
+        <div>
+          <AboutCard>
+            <CardTitle>More than 50.000 clients!</CardTitle>
+            <p>
+              Everyone knows our quality, that's why they choose us. We have the
+              best contacts that exist to provide our clients with the best
+              quality and service possible.
+            </p>
+          </AboutCard>
+          <AboutCard>
+            <CardTitle>Join us at a very low price!</CardTitle>
+            <p>
+              For only $100 a month, you can get our services and not worry
+              about any problems you may have. Just enjoy, we will do the rest
+            </p>
+          </AboutCard>
+        </div>
+        <div style={{ textAlign: "center", margin: "50px" }}>
+          <ImgIcon src={icon} />
+        </div>
+      </CardsContainer>
+    </StyledSection>
+  );
+}
